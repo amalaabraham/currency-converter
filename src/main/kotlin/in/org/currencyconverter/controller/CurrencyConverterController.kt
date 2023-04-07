bbcdc7ab3f1d679617b8e3e61f3b1ff5e9a5d87b
amala <amala.abraham@hatio.in>
2023-04-07 22:55:43 +0530

    add currency converter

---
+++ src/main/kotlin/in/org/currencyconverter/controller/CurrencyConverterController.kt
@@ -0,0 +1,39 @@
+package `in`.org.currencyconverter.controller
+
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import java.math.BigDecimal
+
+data class CurrencyConversionResult(val base: String, val rates: Map<String, BigDecimal>)
+
+@RestController
+class CurrencyConverterController {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("currencies", listOf("EUR", "USD", "GBP"))
+        return "index"
+    }
+
+    @GetMapping("/convert")
+    fun convert(
+        @RequestParam(name = "amount") amount: BigDecimal,
+        @RequestParam(name = "from") from: String,
+        @RequestParam(name = "to") to : String
+    ): String {
+        val url = "https://api.apilayer.com/exchangerates_data/latest?base=$from&symbols=$to"
+        val headers = HttpHeaders()
+        headers.set("apikey", "X2DKZRS0kWixxVCi59925Gd5wUVUVPwu")
+        val entity = HttpEntity<String>(headers)
+        val result = RestTemplate().exchange(url, HttpMethod.GET, entity, CurrencyConversionResult::class.java).body
+        val rate = result?.rates?.get(to)
+        val convertedAmount = amount * rate!!
+        return "$amount $from = $convertedAmount $to"
+    }
+}
---
+++ src/test/kotlin/in/org/currencyconverter/CurrencyconverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package in.org.currencyconverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyconverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
